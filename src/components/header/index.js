ed3a2873f661d1cce68186a2d1a6dd16f37fd9c4
Anil Yadav <anil.yadav@pragmaapps.com>
2023-08-06 16:24:57 +0530

    updated menu

---
+++ src/components/header/index.js
@@ -0,0 +1,34 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "./style.css";
+const logoImage = require("../../assets/images/oafalogo.png"); // Adjust the path as needed
+
+function Header() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#">
+          <img
+            src={logoImage}
+            alt="Logo"
+            width="30"
+            height="auto"
+            className="d-inline-block align-top"
+          />
+          OAFA ACADEMY
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="menu-navbar-nav" />
+        <Navbar.Collapse id="menu-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">OUR ACADEMY</Nav.Link>
+            <Nav.Link href="#link"> OUR VISION </Nav.Link>
+            <Nav.Link href="#link">OUR CLUB</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
